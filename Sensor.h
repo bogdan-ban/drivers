fa96f74eff0d6e395db6c22b4747fc097781a0c8
bogdan-ban <91048327+bogdan-ban@users.noreply.github.com>
2021-09-20 11:29:18 +0300

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -4,10 +4,16 @@
 class Sensor
 {
 protected:
-	std::string comunication_channel;
+	std::string communication_channel;
 
 public:
-	Sensor();
-	Sensor(std::string new_comunication_channel) : std::string new_comunication_channel ("");
+	Sensor() : communication_channel("")
+	{
+	}
+
+	Sensor(std::string new_communication_channel) : communication_channel(new_communication_channel)
+	{
+	}
+
 	virtual void change_path(std::string new_comunication_channel) = 0;
 };
