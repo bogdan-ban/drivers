f9ed7963a54752de2060d4ccddc21d19ad0527d8
bogdan-ban <91048327+bogdan-ban@users.noreply.github.com>
2021-09-20 10:55:51 +0300

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -7,5 +7,10 @@ protected:
 	std::string comunication_channel = "";
 
 public:
+	Sensor();
+	Sensor(std::string new_comunication_channel)
+	{
+		comunication_channel = new_comunication_channel;
+	}
 	virtual void change_path(std::string new_comunication_channel) = 0;
 };
