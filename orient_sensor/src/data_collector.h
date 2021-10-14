e3219e0a5de052b0cff09df89908f94a3d1a0bc7
FIXED-TERM Ban Bogdan (RBRO/PJ-BEG) <bab3clj@bosch.com>
2021-10-14 14:07:59 +0300

    Comments removed

--- orient_sensor/src/data_collector.h
+++ orient_sensor/src/data_collector.h
@@ -26,42 +26,13 @@ class DataCollector
 {
 private:
 	ros::Publisher pub;
-	/*int baud_rate;
-	char* serial_port;
-	char* i2c_port;
-	*/
-
+	
 public:
-	// serialib serial;
 
 	Communication* communication;
 
 	DataCollector(ros::NodeHandle *nh);
 
-	/*int open_UART();
-
-	void print_to_UART(const uint8_t buffer[],int size);
-
-	uint8_t read_from_UART();
-
-	void close_UART();
-
-	int open_I2C();
-
-	void print_to_I2C(int fd, uint8_t buffer[]);
-
-	uint8_t read_from_I2C(int fd);
-
-	void close_I2C(int bus);
-
-	void flush_serial_receiver();
-
-	void read_data_UART(const string opt);
-
-	int convert_bytes_2_int(uint8_t vec[2]);
-
-	void read_data_I2C(int bus, const string opt);
-	*/
 };
 
 #endif
